701300fc20e921bc2d7505d480f6519a4fbfa194
Nick Zadrozny <nick@beyondthepath.com>
2013-02-08 12:07:21 -0700

    Basic testing/benchmarking script.

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+  curl -s -X POST http://localhost:5000/test/test/`openssl rand -base64 10 | tr -cd '[:alnum:]'` \
+    --data-binary "{now:\"`date`\", rand:\"`openssl rand -base64 10`\"}" && echo -n . || true
+done
