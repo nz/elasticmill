2d7274426c8bfb43dbf54f38deb04c64ea2660e0
Nick Zadrozny <nick@beyondthepath.com>
2013-02-08 17:39:04 -0700

    Fix the json in my test

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ fi
 
 # Send over a bunch of random documents
 while true; do
-  curl -s -o /dev/null -X POST "$url/test/test/`openssl rand -base64 10 | tr -cd '[:alnum:]'`" \
-    --data-binary "{now:\"`date`\", rand:\"`openssl rand -base64 10`\"}" && echo -n . || true
+  curl -s -X POST "$url/test/test/`openssl rand -base64 10 | tr -cd '[:alnum:]'`" \
+    --data-binary "{ \"now\":\"`date`\", \"rand\":\"`openssl rand -base64 10`\"}" > /dev/null && echo -n . || true
 done
